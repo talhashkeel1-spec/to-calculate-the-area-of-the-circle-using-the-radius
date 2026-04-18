32a699483f6aec830df9589c6747741ba58b5d29
Muhammad Talha <talhashkeel1@gmail.com>
2026-04-18 21:25:19 +0500

    to calculate the area of the circle using the radius

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+float radius,area;
+cout<< "Enter radius of circle:";
+cin>>radius;
+area = 3.14*radius*radius;
+cout<<"Area of the circle is:"<<area<< end|;
+return 0;
+}
+
+
